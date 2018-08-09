69f6a21cddc27131daf24f1a155367d9ed666dab
mvboettcher <mvboettcher@gmail.com>
2018-08-09 11:09:57 -0400

    added dependencies

--- app/src/action-creators/breweries.js
+++ app/src/action-creators/breweries.js
@@ -1,11 +1,10 @@
-import { SET_BREWERIES } from '../constants'
 import fetch from 'isomorphic-fetch'
+import { SET_BREWERIES } from '../constants'
 const url = process.env.REACT_APP_BASE_URL + '/breweries'
 
-export const setBreweries = async (dispatch, setState) => {
+export const setBreweries = async (dispatch, getState) => {
   const breweries = await fetch(url)
     .then(res => res.json())
     .catch(err => console.log(err))
-
   dispatch({ type: SET_BREWERIES, payload: breweries })
 }
--- app/src/index.js
+++ app/src/index.js
@@ -2,15 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 import store from './store'
 import { Provider } from 'react-redux'
 import { setBreweries } from './action-creators/breweries'
 
 ReactDOM.render(
   <Provider store={store}>
-    <App dispatch={store.dispatch} />
+    <App />
   </Provider>,
   document.getElementById('root')
 )
+registerServiceWorker()
 
 store.dispatch(setBreweries)
